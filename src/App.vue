8a91e69d50fc80c18756f5a40dce9c4b7aa4247b
Gaaston14 <gaston.ignacio.gimenez@gmail.com>
2023-05-06 17:44:30 -0300

    cambios varios

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,82 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img alt="Vuetify Logo" class="shrink mr-2" contain src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png" transition="scale-transition" width="40" />
+        <v-img alt="Vuetify Name" class="shrink mt-1 hidden-sm-and-down" contain min-width="100" src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png" width="100" />
+      </div>
+      <v-spacer></v-spacer>
+      <v-menu v-for="(item, index) in rutas2" :key="index" :close-on-content-click="false" transition="scale-transition" offset-y>
+        <template v-slot:activator="{ on }">
+          <v-btn text v-on="on">
+            <v-icon v-if="item.icono">{{ item.icono }}</v-icon>
+            {{ item.nombre }}            
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item v-for="(subItem, subIndex) in item.subitems" :key="subIndex" @click="insertarRuta(subItem.ruta)">
+            <v-list-item-title>
+              <v-icon v-if="subItem.icono">{{ subItem.icono }}</v-icon>
+              {{ subItem.nombre }}
+            </v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+export default {
+  name: "App",
+  data: () => ({
+    rutas2: [
+      {
+        nombre: "Tecnicos",
+        icono: "mdi-account",
+        subitems: [
+          { nombre: "Crear Tecnico", ruta: "/", icono: "mdi-account-plus"},
+          { nombre: "Modificar Tecnico", ruta: "/about", icono: "mdi-account-edit" },
+          { nombre: "Listar Tecnicos", ruta: "/usuarios", icono: "mdi-list-box" }
+        ]
+      },
+      {
+        nombre: "Grupos",
+        icono: "mdi-account-group",
+        subitems: [
+          { nombre: "Crear Grupo", ruta: "/", icono: "mdi-account-multiple-plus" },
+          { nombre: "Modificar Grupo", ruta: "/about", icono: "mdi-pencil" },
+          { nombre: "Listar Grupos", ruta: "/usuarios", icono: "mdi-list-box" }
+        ]
+      },
+      {
+        nombre: "Tareas",
+        icono: "mdi-format-list-checkbox",
+        subitems: [
+          { nombre: "Agregar Tarea", ruta: "/", icono: "mdi-plus" },
+          { nombre: "Modificar Tareas", ruta: "/about", icono: "mdi-pencil" },
+          { nombre: "Modificar Precio", ruta: "/about", icono: "mdi-currency-usd" },
+          { nombre: "Listar Tareas", ruta: "/usuarios", icono: "mdi-list-box" },
+        ]
+      },
+      {
+        nombre: "Certificar Tareas",
+        icono: "mdi-cash",
+        subitems: [
+          { nombre: "Nueva Certificacion", ruta: "/", icono: "mdi-cash-plus" },
+          { nombre: "Consultar Certificacion", ruta: "/about", icono: "mdi-pencil" },
+          { nombre: "Listar Certifiaciones", ruta: "/usuarios", icono: "mdi-view-list" }
+        ]
+      },
+    ]
+  }),
+  methods: {
+    insertarRuta(ruta) {
+      this.$router.push(ruta);
+    }
+  }
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import UsuariosView from '../views/UsuariosView.vue'
+
 
 Vue.use(VueRouter)
 
@@ -13,13 +16,13 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-    }
-  }
+    component: AboutView
+    },
+  {
+      path: '/usuarios',
+      name: 'usuarios',
+      component: UsuariosView
+  },
 ]
 
 const router = new VueRouter({
