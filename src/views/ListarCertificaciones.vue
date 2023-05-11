67aa1367a20f0a789cf11e08ddb048413ee9c7e5
Gaaston14 <gaston.ignacio.gimenez@gmail.com>
2023-05-11 19:37:27 -0300

    asda

--- src/views/ListarCertificaciones.vue
+++ src/views/ListarCertificaciones.vue
@@ -1,50 +1,95 @@
 <template>
-  <v-container fluid>
-    <v-row>
-      <v-col v-for="(item, index) in items" :key="index" cols="3">
-        <v-card outlined>
-          <v-card-title>{{ item.nombre }}</v-card-title>
-          <v-card-text>
-            <v-row>
-              <v-col cols="6">Código:</v-col>
-              <v-col cols="6">{{ item.id }}</v-col>
-            </v-row>
-            <v-row>
-              <v-col cols="6">Precio:</v-col>
-              <v-col cols="6">{{ item.precio }}</v-col>
-            </v-row>
-            <v-row>
-              <v-col cols="6">Última fecha de modificación:</v-col>
-              <v-col cols="6">{{ item.fechaDesde.substring(0, 10) }}</v-col>
-            </v-row>
-          </v-card-text>
-          <v-card-actions>
-            <v-btn color="primary">Modificar</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col cols="12" md="6">
+            <v-menu v-model="menu1" :close-on-content-click="false" transition="scale-transition" offset-y max-width="290px">
+              <template v-slot:activator="{ on }">
+                <v-text-field v-model="fechaInicio" label="Fecha de inicio" prepend-icon="mdi-calendar" readonly v-on="on"></v-text-field>
+              </template>
+              <v-date-picker v-model="fechaInicio" no-title scrollable @input="menu1 = false"></v-date-picker>
+            </v-menu>
+          </v-col>
+          <v-col cols="12" md="6">
+            <v-menu v-model="menu2" :close-on-content-click="false" transition="scale-transition" offset-y max-width="290px">
+              <template v-slot:activator="{ on }">
+                <v-text-field v-model="fechaFin" label="Fecha de fin" prepend-icon="mdi-calendar" readonly v-on="on"></v-text-field>
+              </template>
+              <v-date-picker v-model="fechaFin" no-title scrollable @input="menu2 = false"></v-date-picker>
+            </v-menu>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-simple-table>
+              <template v-slot:default>
+                <thead>
+                  <tr>
+                    <th>Id Grupo</th>
+                    <th>Conexion</th>
+                    <th>Id Tarea</th>
+                    <th>Fecha de Cumplimiento</th>
+                    <th>Cantidad</th>
+                    <th>Hora</th>
+                    <th>Observacion</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="certificacion in certificaciones" :key="certificacion.idtarea">
+                    <td>{{ certificacion.idgrupo }}</td>
+                    <td>{{ certificacion.conexion }}</td>
+                    <td>{{ certificacion.idtarea }}</td>
+                    <td>{{ certificacion.fechaCumplimiento }}</td>
+                    <td>{{ certificacion.cantidad }}</td>
+                    <td>{{ certificacion.hora }}</td>
+                    <td>{{ certificacion.observacion }}</td>
+                  </tr>
+                </tbody>
+              </template>
+            </v-simple-table>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
-
 <script>
+import axios from 'axios'
+
 export default {
-  name: "Tareas",
   data() {
     return {
-      items: [],
-    };
-  },
-  mounted() {
-    this.fetchData();
+      menu1: false,
+      menu2: false,
+      fechaInicio: '',
+      fechaFin: '',
+      certificaciones: []
+    }
   },
   methods: {
-    async fetchData() {
-      const url = "http://localhost:4000/api/tareas/tareaprecio";
-      const response = await fetch(url);
-      const data = await response.json();
-      this.items = data.body;
-    },
+    async consultarCertificaciones() {
+      try {
+        const body = {
+          f1: this.fechaInicio,
+          f2: this.fechaFin
+        }
+        const response = await axios.get('http://localhost:4000/api/tareas/ListarCertificaciones', {
+          params: body
+        })
+        this.certificaciones = response.data
+      } catch (error) {
+        console.error(error)
+      }
+    }
   },
-};
+  watch: {
+    fechaInicio() {
+      this.consultarCertificaciones()
+    },
+    fechaFin() {
+      this.consultarCertificaciones()
+    }
+  }
+}
 </script>
