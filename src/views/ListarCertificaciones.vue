cfd3cdce9d47d689d3c8f40fe5188ea6809b6874
Gaaston14 <gaston.ignacio.gimenez@gmail.com>
2023-05-13 17:02:43 -0300

    todo ok

--- src/views/ListarCertificaciones.vue
+++ src/views/ListarCertificaciones.vue
@@ -1,95 +1,96 @@
 <template>
-  <v-app>
-    <v-main>
-      <v-container>
-        <v-row>
-          <v-col cols="12" md="6">
-            <v-menu v-model="menu1" :close-on-content-click="false" transition="scale-transition" offset-y max-width="290px">
-              <template v-slot:activator="{ on }">
-                <v-text-field v-model="fechaInicio" label="Fecha de inicio" prepend-icon="mdi-calendar" readonly v-on="on"></v-text-field>
-              </template>
-              <v-date-picker v-model="fechaInicio" no-title scrollable @input="menu1 = false"></v-date-picker>
-            </v-menu>
-          </v-col>
-          <v-col cols="12" md="6">
-            <v-menu v-model="menu2" :close-on-content-click="false" transition="scale-transition" offset-y max-width="290px">
-              <template v-slot:activator="{ on }">
-                <v-text-field v-model="fechaFin" label="Fecha de fin" prepend-icon="mdi-calendar" readonly v-on="on"></v-text-field>
-              </template>
-              <v-date-picker v-model="fechaFin" no-title scrollable @input="menu2 = false"></v-date-picker>
-            </v-menu>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col>
-            <v-simple-table>
-              <template v-slot:default>
-                <thead>
-                  <tr>
-                    <th>Id Grupo</th>
-                    <th>Conexion</th>
-                    <th>Id Tarea</th>
-                    <th>Fecha de Cumplimiento</th>
-                    <th>Cantidad</th>
-                    <th>Hora</th>
-                    <th>Observacion</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr v-for="certificacion in certificaciones" :key="certificacion.idtarea">
-                    <td>{{ certificacion.idgrupo }}</td>
-                    <td>{{ certificacion.conexion }}</td>
-                    <td>{{ certificacion.idtarea }}</td>
-                    <td>{{ certificacion.fechaCumplimiento }}</td>
-                    <td>{{ certificacion.cantidad }}</td>
-                    <td>{{ certificacion.hora }}</td>
-                    <td>{{ certificacion.observacion }}</td>
-                  </tr>
-                </tbody>
-              </template>
-            </v-simple-table>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-main>
-  </v-app>
+  <v-container>
+    <v-row>
+      <v-col cols="12" md="6">
+        <v-text-field v-model="fechaInicio" label="Fecha de inicio" @click="abrirFechaInicio"></v-text-field>
+        <v-date-picker v-model="fechaInicio" v-if="mostrarFechaInicio" @input="cerrarFechaInicio"></v-date-picker>
+      </v-col>
+      <v-col cols="12" md="6">
+        <v-text-field v-model="fechaFin" label="Fecha de fin" @click="abrirFechaFin"></v-text-field>
+        <v-date-picker v-model="fechaFin" v-if="mostrarFechaFin" @input="cerrarFechaFin"></v-date-picker>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" md="6">
+        <v-select v-model="tecnicoSeleccionado" :items="tecnicos" label="Seleccionar Técnico" item-text="nombre" item-value="id"></v-select>
+      </v-col>
+      <v-col cols="12" md="6">
+        <v-btn @click="consultarTareas" color="primary">Consultar Tareas</v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <v-data-table :headers="headers" :items="tareas" hide-default-footer></v-data-table>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 <script>
-import axios from 'axios'
+import axios from 'axios';
 
 export default {
   data() {
     return {
-      menu1: false,
-      menu2: false,
-      fechaInicio: '',
-      fechaFin: '',
-      certificaciones: []
-    }
+      fechaInicio: null,
+      fechaFin: null,
+      tecnicoSeleccionado: null,
+      tecnicos: [],
+      tareas: [],
+      mostrarFechaInicio: false,
+      mostrarFechaFin: false,
+      headers: [
+        { text: 'Nombre', value: 'nombre' },
+        { text: 'Conexión', value: 'conexion' },
+        { text: 'Fecha de Cumplimiento', value: 'fechaCumplimiento' },
+        { text: 'Hora', value: 'hora' },
+        { text: 'Tarea', value: 'tarea' },
+        { text: 'Cantidad', value: 'cantidad' },
+        { text: 'Observación', value: 'observacion' },
+      ],
+    };
+  },
+  mounted() {
+    this.obtenerTecnicos();
   },
   methods: {
-    async consultarCertificaciones() {
+    async obtenerTecnicos() {
       try {
-        const body = {
-          f1: this.fechaInicio,
-          f2: this.fechaFin
-        }
-        const response = await axios.get('http://localhost:4000/api/tareas/ListarCertificaciones', {
-          params: body
-        })
-        this.certificaciones = response.data
+        const response = await axios.get('http://localhost:4000/api/tecnicos/asignados');
+        this.tecnicos = response.data.body;
       } catch (error) {
-        console.error(error)
+        console.error(error);
       }
-    }
-  },
-  watch: {
-    fechaInicio() {
-      this.consultarCertificaciones()
     },
-    fechaFin() {
-      this.consultarCertificaciones()
-    }
-  }
-}
+    abrirFechaInicio() {
+      this.mostrarFechaInicio = true;
+    },
+    abrirFechaFin() {
+      this.mostrarFechaFin = true;
+    },
+    cerrarFechaInicio() {
+      this.mostrarFechaInicio = false;
+    },
+    cerrarFechaFin() {
+      this.mostrarFechaFin = false;
+    },
+    async consultarTareas() {
+      if (!this.fechaInicio || !this.fechaFin || !this.tecnicoSeleccionado) {
+        return;
+      }
+      
+      const requestData = {
+        f1: this.fechaInicio,
+        f2: this.fechaFin,
+        id: this.tecnicoSeleccionado,
+      };
+      
+      try {
+        const response = await axios.post('http://localhost:4000/api/tareas/ListarCertificaciones', requestData);
+        this.tareas = response.data.body;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+};
 </script>
