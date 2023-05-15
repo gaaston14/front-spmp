1fbe04209cbd718e0fa177f72d5220bb2d96c60e
Gaston Gimenez <gaston.ignacio.gimenez@gmail.com>
2023-05-15 15:23:27 -0300

    se actualiza vista pirncipal

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,64 @@
 <template>
-  <hello-world />
+  <div>
+    <highcharts :options="chartOptions"></highcharts>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+import { Chart } from 'highcharts-vue';
+import axios from 'axios';
 
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
+export default {
+  components: {
+    highcharts: Chart,
+  },
+  data() {
+    return {
+      chartOptions: {
+        chart: {
+          type: 'pie',
+        },
+        title: {
+          text: 'Tareas realizadas en los ultimos 30 dias',
+        },
+        series: [],
+        tooltip: {
+          pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>',
+        },
+        plotOptions: {
+          pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+              enabled: true,
+              format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+            },
+          },
+        },
+      },
+    };
+  },
+  mounted() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      axios
+        .get('http://localhost:4000/api/tareas/sumatareas')
+        .then(response => {
+          const data = response.data.body;
+          this.chartOptions.series = [{
+            name: 'Porcentaje',
+            data: data.map(item => ({
+              name: item.nombre,
+              y: item.cantidad,
+            })),
+          }];
+        })
+        .catch(error => {
+          console.error(error);
+        });
     },
-  }
+  },
+};
 </script>
